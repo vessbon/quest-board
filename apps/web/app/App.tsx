07bcde9569c0d758196cd254aa805910567d2d2a
Anton Norgren <anton.norgren06@gmail.com>
2025-11-07 18:28:15 +0100

    chore: cleanup css and exports

--- apps/web/app/App.tsx
+++ apps/web/app/App.tsx
@@ -1,11 +1,7 @@
-import "./App.css";
-import { Button } from "@repo/ui";
-
 function App() {
   return (
     <>
       <h1>Hello</h1>
-      <Button children="Click me" appName="Quest Board" />
     </>
   );
 }
