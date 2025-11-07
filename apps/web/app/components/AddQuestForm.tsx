d77ea5370253eade828c4ff3ec565218deeda13f
Anton Norgren <anton.norgren06@gmail.com>
2025-11-07 22:16:51 +0100

    feat: add quest

---
+++ apps/web/app/components/AddQuestForm.tsx
@@ -0,0 +1,45 @@
+import "../index.css";
+import type { Quest } from "../types";
+import { useState } from "react";
+
+interface AddQuestFormProps {
+  addQuest: (title: Quest["id"]) => void;
+}
+
+export default function AddQuestForm({ addQuest }: AddQuestFormProps) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!title.trim()) return;
+    addQuest(title);
+    setTitle("");
+  };
+
+  return (
+    <div className="flex min-h-screen justify-center items-center text-center">
+      <form className="max-w-md min-w-xs w-1/3" onSubmit={handleSubmit}>
+        <label className="font-bold mb-4 text-4xl block" htmlFor="quest-first">
+          Quest
+        </label>
+        <input
+          className="border p-2 w-full text-center text-lg placeholder-gray-500 outline-none mb-2"
+          name="title"
+          id="quest-first"
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          className="btn-primary p-1 w-full"
+          name="submit"
+          id="submit-quest"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
