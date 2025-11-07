3ce496cf4c6a25f742fc1bee10487ff98768d26c
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-07 17:54:45 +0100

    initial commit

---
+++ apps/web/app/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@repo/ui";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button children="Click me" appName="Quest Board" />
+    </>
+  );
+}
+
+export default App;
