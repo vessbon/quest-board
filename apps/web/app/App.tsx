d545edae62529c91385c806d4849284dff6aaa09
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-08 21:02:18 +0100

    feat: toggle complete and delete quest

--- apps/web/app/App.tsx
+++ apps/web/app/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { Quest } from "./types";
 import AddQuestForm from "./components/AddQuestForm";
+import QuestCard from "./components/QuestCard";
 
 function App() {
   const [quests, setQuests] = useState<Quest[]>([]);
@@ -13,9 +14,29 @@ function App() {
     console.log(quests);
   };
 
+  const toggleComplete = (id: Quest["id"]) => {
+    setQuests(
+      quests.map((quest) =>
+        quest.id === id ? { ...quest, completed: !quest.completed } : quest
+      )
+    );
+  };
+
+  const deleteQuest = (id: Quest["id"]) => {
+    setQuests(quests.filter((quest) => quest.id !== id));
+  };
+
   return (
     <>
       <AddQuestForm addQuest={addQuest} />
+      {quests.length > 0 && (
+        <QuestCard
+          key={quests.at(-1)!.id} // non-null assertion because length > 0
+          quest={quests.at(-1)!} // same here
+          toggleComplete={toggleComplete}
+          deleteQuest={deleteQuest}
+        />
+      )}
     </>
   );
 }
