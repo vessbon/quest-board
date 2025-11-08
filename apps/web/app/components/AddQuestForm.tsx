bde1a9cecafabc490534a413aada735f00374196
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-08 23:16:36 +0100

    refactor: remove unnecessary div

--- apps/web/app/components/AddQuestForm.tsx
+++ apps/web/app/components/AddQuestForm.tsx
@@ -17,24 +17,25 @@ export default function AddQuestForm({ addQuest }: AddQuestFormProps) {
   };
 
   return (
-    <div className="flex min-h-screen justify-center items-center text-center">
-      <form className="max-w-md min-w-xs w-1/3" onSubmit={handleSubmit}>
-        <label className="font-bold mb-4 text-4xl block" htmlFor="quest-first">
-          Quest
-        </label>
-        <input
-          className="border p-2 w-full text-center text-lg placeholder-gray-500 outline-none mb-2"
-          name="title"
-          id="quest-first"
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <button className="btn-primary p-1 w-full" name="submit" type="submit">
-          Add
-        </button>
-      </form>
-    </div>
+    <form
+      className="max-w-md min-w-xs w-1/3 text-center"
+      onSubmit={handleSubmit}
+    >
+      <label className="font-bold mb-4 text-4xl block" htmlFor="quest-first">
+        Quest
+      </label>
+      <input
+        className="border p-2 w-full text-center text-lg outline-none mb-2"
+        name="title"
+        id="quest-first"
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button className="btn-primary p-1 w-full" name="submit" type="submit">
+        Add
+      </button>
+    </form>
   );
 }
