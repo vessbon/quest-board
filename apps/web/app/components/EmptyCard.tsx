bd2d727a16887479b3e92d41272f42fdc29399a6
Anton Norgren <anton.norgren06@gmail.com>
2025-11-09 11:21:30 +0100

    feat: display not found card

---
+++ apps/web/app/components/EmptyCard.tsx
@@ -0,0 +1,8 @@
+export default function EmptyCard() {
+  return (
+    <div className="card-primary border-dashed! border-info text-warning">
+      <h1 className="text-lg">No Quests Found</h1>
+      <p>Try adding one with the form above.</p>
+    </div>
+  );
+}
