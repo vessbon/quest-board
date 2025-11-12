624e7f8a6589d37c159dcc62900061a781b484ef
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-12 18:39:44 +0100

    feat: kysely db connection

---
+++ apps/api/src/db/index.ts
@@ -0,0 +1,14 @@
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+import type { DB } from './types';
+
+import dotenv from 'dotenv';
+require('dotenv').config;
+
+export const db = new Kysely<DB>({
+    dialect: new PostgresDialect({
+        pool: new Pool({
+            connectionString: process.env.DATABASE_URL
+        })
+    })
+})
