fb88eb0578cfac1f624e4fff67fd62d5f06154e3
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-12 19:16:11 +0100

    feat: get request for quests

--- apps/api/src/routes/quests.ts
+++ apps/api/src/routes/quests.ts
@@ -2,3 +2,8 @@ import { Hono } from "hono";
 import { db } from "../db";
 
 export const questsRoute = new Hono();
+
+questsRoute.get("/", async (c) => {
+  const quests = await db.selectFrom("quest").selectAll().execute();
+  return c.json(quests);
+});
