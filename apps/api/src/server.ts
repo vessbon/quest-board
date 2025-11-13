3d3bfa513e10fb3c5efa5e731bf8becdd8c64b32
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-14 00:02:47 +0100

    fix: handle cors

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "./router";
 
 const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(
   "/trpc/*",
