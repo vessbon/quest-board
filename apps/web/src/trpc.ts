02e596f03d966db38dd2f4e1fdb2a0987d7f5f13
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-14 00:03:30 +0100

    feat: superjson for backend

--- apps/web/src/trpc.ts
+++ apps/web/src/trpc.ts
@@ -1,12 +1,18 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
-import type { AppRouter } from "../../../api/src/router";
+import type { AppRouter } from "../../api/src/router";
+import superjson from "superjson";
 
 export const queryClient = new QueryClient();
 
 export const trpcClient = createTRPCClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://localhost:3000" })],
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000/trpc",
+      transformer: superjson,
+    }),
+  ],
 });
 
 export const trpc = createTRPCOptionsProxy<AppRouter>({
