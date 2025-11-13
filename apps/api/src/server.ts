4af56f738d886b5640059804455fa6c750224d93
vessbon <115010176+vessbon@users.noreply.github.com>
2025-11-13 20:01:26 +0100

    refactor: use trpc

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,34 @@
+import { db } from "./db";
 import { Hono } from "hono";
-import { questsRoute } from "./routes/quests";
+import { trpcServer } from "@hono/trpc-server";
+import { router, publicProcedure } from "./trpc";
 
 const app = new Hono();
-app.route("/quests", questsRoute);
 
+const appRouter = router({
+  questList: publicProcedure.query(async () => {
+    const quests = await db.selectFrom("quest").selectAll().execute();
+    return quests;
+  }),
+
+  questById: publicProcedure
+    .input((val: unknown) => {
+      if (typeof val === "number") return val;
+      throw new Error(`Invalid input: ${typeof val}`);
+    })
+    .query(async (opts) => {
+      const { input } = opts;
+      const quest = await db.selectFrom("quest").where("id", "=", `${input}`);
+      return quest;
+    }),
+});
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+  })
+);
+
+export type AppRouter = typeof appRouter;
 export default app;
